999c7c9a72b52a5674a58c4c1cf620b278e580ab
SKairinos <stefan.kairinos@ocado.com>
2024-06-06 15:27:01 +0000

    reference src

--- main.py
+++ main.py
@@ -6,4 +6,4 @@ Created on 11/04/2024 at 16:51:45(+01:00).
 # This is the entrypoint to our app.
 # https://cloud.google.com/appengine/docs/standard/python3/runtime#application_startup
 # pylint: disable-next=unused-import
-from service.wsgi import application as app
+from src.service.wsgi import application as app
