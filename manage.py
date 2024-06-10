a9da95c87d7c3257010e79782ed6c067a40f75d0
SKairinos <stefan.kairinos@ocado.com>
2024-06-10 08:14:47 +0000

    move service configs to root

--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "src.service.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
     try:
         # pylint: disable-next=import-outside-toplevel
         from django.core.management import execute_from_command_line
--- settings.py
+++ settings.py
@@ -22,7 +22,7 @@ DOTDIGITAL_CAMPAIGN_IDS = {
 }
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 
 # Custom
 LOGIN_REDIRECT_URL = "/teach/dashboard/"
