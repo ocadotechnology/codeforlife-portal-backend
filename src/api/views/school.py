7e9f8c8495bd457194ec0ba3f65d21cf0988c15f
Stefan Kairinos <skairinos@gmail.com>
2024-06-25 10:44:28 +0100

    Delete school (#350)
    * move service configs to root
    
    * fix launch
    
    * move scripts to dir
    
    * load fixtures
    
    * fix login endpoints
    
    * add user type to session metadata
    
    * install new py package
    
    * sync config
    
    * fix auth tests
    
    * send verify email address campaigns
    
    * fix type
    
    * reuse campaign names
    
    * fix scripts
    
    * send release student email
    
    * install new package
    
    * fix signals
    
    * ignore duplicate code
    
    * fix linting error
    
    * reset password
    
    * reject join class request
    
    * join class request notification
    
    * join class request notification
    
    * Student join request sent
    
    * teacher admin given and revoked
    
    * Invite teacher - account doesn't exist
    
    * Invite teacher - account exists
    
    * User already registered
    
    * Teacher released from school
    
    * Email change notification
    
    * fix type
    
    * Verify changed user email
    
    * fix imports
    
    * Account deletion
    
    * new package and fix test
    
    * enable dotdigital
    
    * import all signals
    
    * new py package
    
    * do no allow email enumeration
    
    * stop email enumeration
    
    * feedback
    
    * do no send when indy joins class
    
    * fix linting errors
    
    * fix linting errors
    
    * install new py package
    
    * anonymize school
    
    * release teachers from school

--- src/api/views/school.py
+++ src/api/views/school.py
@@ -4,15 +4,18 @@ Created on 23/01/2024 at 17:53:50(+00:00).
 """
 
 from codeforlife.permissions import AllowNone
+from codeforlife.response import Response
+from codeforlife.user.models import Class, StudentUser, Teacher
 from codeforlife.user.permissions import IsTeacher
 from codeforlife.user.views import SchoolViewSet as _SchoolViewSet
+from rest_framework import status
 
 from ..serializers import SchoolSerializer
 
 
 # pylint: disable-next=missing-class-docstring,too-many-ancestors
 class SchoolViewSet(_SchoolViewSet):
-    http_method_names = ["get", "post", "patch"]
+    http_method_names = ["get", "post", "patch", "delete"]
     serializer_class = SchoolSerializer
 
     def get_permissions(self):
@@ -22,8 +25,25 @@ class SchoolViewSet(_SchoolViewSet):
         # Only teachers not in a school can create a school.
         if self.action == "create":
             return [IsTeacher(in_school=False)]
-        # Only admin-teachers in a school can update a school.
-        if self.action == "partial_update":
+        # Only admin-teachers in a school can update or delete a school.
+        if self.action in ["partial_update", "destroy"]:
             return [IsTeacher(is_admin=True)]
 
         return super().get_permissions()
+
+    def destroy(self, request, *args, **kwargs):
+        school = self.get_object()
+
+        for klass in Class.objects.filter(teacher__school=school):
+            for student_user in StudentUser.objects.filter(
+                new_student__class_field=klass
+            ):
+                student_user.anonymize()
+
+            klass.anonymise()
+
+        Teacher.objects.filter(school=school).update(school=None)
+
+        school.anonymise()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
--- src/api/views/school_test.py
+++ src/api/views/school_test.py
@@ -3,9 +3,20 @@
 Created on 02/02/2024 at 15:31:21(+00:00).
 """
 
+import typing as t
+from unittest.mock import PropertyMock, patch
+
 from codeforlife.permissions import OR, AllowNone
 from codeforlife.tests import ModelViewSetTestCase
-from codeforlife.user.models import School, User
+from codeforlife.user.models import (
+    AdminSchoolTeacherUser,
+    Class,
+    NonSchoolTeacherUser,
+    School,
+    StudentUser,
+    Teacher,
+    User,
+)
 from codeforlife.user.permissions import IsStudent, IsTeacher
 
 from .school import SchoolViewSet
@@ -17,9 +28,18 @@ class TestSchoolViewSet(ModelViewSetTestCase[User, School]):
     model_view_set_class = SchoolViewSet
     fixtures = ["non_school_teacher", "school_1"]
 
+    def setUp(self):
+        self.non_school_teacher_user = NonSchoolTeacherUser.objects.get(
+            email="teacher@noschool.com"
+        )
+        self.admin_school_teacher_user = AdminSchoolTeacherUser.objects.get(
+            email="admin.teacher@school1.com"
+        )
+
+    # test: get permissions
+
     def test_get_permissions__bulk(self):
         """No one is allowed to perform bulk actions."""
-
         self.assert_get_permissions(
             permissions=[AllowNone()],
             action="bulk",
@@ -27,7 +47,6 @@ class TestSchoolViewSet(ModelViewSetTestCase[User, School]):
 
     def test_get_permissions__list(self):
         """No one is allowed to list schools."""
-
         self.assert_get_permissions(
             permissions=[AllowNone()],
             action="list",
@@ -35,7 +54,6 @@ class TestSchoolViewSet(ModelViewSetTestCase[User, School]):
 
     def test_get_permissions__create(self):
         """Only teachers not in a school can create a school."""
-
         self.assert_get_permissions(
             permissions=[IsTeacher(in_school=False)],
             action="create",
@@ -43,7 +61,6 @@ class TestSchoolViewSet(ModelViewSetTestCase[User, School]):
 
     def test_get_permissions__partial_update(self):
         """Only admin-teachers in a school can update a school."""
-
         self.assert_get_permissions(
             permissions=[IsTeacher(is_admin=True)],
             action="partial_update",
@@ -51,20 +68,23 @@ class TestSchoolViewSet(ModelViewSetTestCase[User, School]):
 
     def test_get_permissions__retrieve(self):
         """Anyone in a school can retrieve a school."""
-
         self.assert_get_permissions(
             permissions=[OR(IsStudent(), IsTeacher(in_school=True))],
             action="retrieve",
         )
 
-    def test_create(self):
-        """Can successfully create a school."""
-
-        self.client.login_non_school_teacher(
-            email="teacher@noschool.com",
-            password="password",
+    def test_get_permissions__destroy(self):
+        """Only admin-teachers in a school can destroy a school."""
+        self.assert_get_permissions(
+            permissions=[IsTeacher(is_admin=True)],
+            action="destroy",
         )
 
+    # test: actions
+
+    def test_create(self):
+        """Can successfully create a school."""
+        self.client.login_as(self.non_school_teacher_user)
         self.client.create(
             {
                 "name": "ExampleSchool",
@@ -75,12 +95,9 @@ class TestSchoolViewSet(ModelViewSetTestCase[User, School]):
 
     def test_partial_update(self):
         """Can successfully update a school."""
+        user = self.admin_school_teacher_user
 
-        user = self.client.login_admin_school_teacher(
-            email="admin.teacher@school1.com",
-            password="password",
-        )
-
+        self.client.login_as(user)
         self.client.partial_update(
             user.teacher.school,
             {
@@ -89,3 +106,50 @@ class TestSchoolViewSet(ModelViewSetTestCase[User, School]):
                 "country": "GB",
             },
         )
+
+    def test_destroy(self):
+        """
+        Can successfully anonymize a school, including all of its students,
+        teachers and classes.
+        """
+
+        def assert_user_is_anonymized(user: User):
+            assert user.first_name == ""
+            assert user.last_name == ""
+            assert user.email == ""
+            assert user.username == ""
+            assert not user.is_active
+
+        user = self.admin_school_teacher_user
+        school = user.teacher.school
+        teachers: t.List[Teacher] = list(school.teacher_school.all())
+
+        self.client.login_as(user)
+
+        anonymized_name = "abc"
+        with patch(
+            "uuid.UUID.hex",
+            new_callable=PropertyMock,
+            return_value=anonymized_name,
+        ):
+            self.client.destroy(school)
+
+        school.refresh_from_db()
+        assert school.name == anonymized_name
+        assert not school.is_active
+
+        for klass in Class.objects.filter(teacher__school=school):
+            for student_user in StudentUser.objects.filter(
+                new_student__class_field=klass
+            ):
+                assert_user_is_anonymized(student_user)
+
+            assert klass.name == anonymized_name
+            assert klass.access_code == ""
+            assert not klass.is_active
+
+        for teacher in teachers:
+            teacher.refresh_from_db()
+            assert not teacher.school
+
+        school.anonymise()
