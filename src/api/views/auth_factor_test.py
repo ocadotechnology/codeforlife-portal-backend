58f563fe4571eb0c0d04843d7302243697601aef
Stefan Kairinos <skairinos@gmail.com>
2025-01-20 16:22:25 +0000

    fix: otp flow (#388)
    * fix: otp flow
    
    * fix: new cfl package
    
    * fix: create auth factor

--- src/api/views/auth_factor_test.py
+++ src/api/views/auth_factor_test.py
@@ -3,6 +3,9 @@
 Created on 23/01/2024 at 11:22:16(+00:00).
 """
 
+from unittest.mock import patch
+
+import pyotp
 from codeforlife.permissions import AllowNone
 from codeforlife.tests import ModelViewSetTestCase
 from codeforlife.user.models import (
@@ -13,6 +16,7 @@ from codeforlife.user.models import (
     User,
 )
 from codeforlife.user.permissions import IsTeacher
+from pyotp import TOTP
 
 from .auth_factor import AuthFactorViewSet
 
@@ -33,9 +37,9 @@ class TestAuthFactorViewSet(ModelViewSetTestCase[User, AuthFactor]):
 
     # test: get queryset
 
-    def test_get_queryset__admin(self):
+    def test_get_queryset__list__admin(self):
         """
-        Can query the author factors of all teachers in your school if you are
+        Can list the author factors of all teachers in your school if you are
         an admin.
         """
         user = self.mfa_non_admin_school_teacher_user
@@ -45,6 +49,7 @@ class TestAuthFactorViewSet(ModelViewSetTestCase[User, AuthFactor]):
         assert admin_school_teacher_user
 
         self.assert_get_queryset(
+            action="list",
             values=list(
                 user.auth_factors.all()
                 | admin_school_teacher_user.auth_factors.all()
@@ -54,11 +59,54 @@ class TestAuthFactorViewSet(ModelViewSetTestCase[User, AuthFactor]):
             ),
         )
 
-    def test_get_queryset__non_admin(self):
-        """Can only query your own auth factors if you are not an admin."""
+    def test_get_queryset__list__non_admin(self):
+        """Can only list your own auth factors if you are not an admin."""
         user = self.mfa_non_admin_school_teacher_user
 
         self.assert_get_queryset(
+            action="list",
+            values=list(user.auth_factors.all()),
+            request=self.client.request_factory.get(user=user),
+        )
+
+    def test_get_queryset__destroy__admin(self):
+        """
+        Can destroy the author factors of all teachers in your school if you are
+        an admin.
+        """
+        user = self.mfa_non_admin_school_teacher_user
+        admin_school_teacher_user = AdminSchoolTeacherUser.objects.filter(
+            new_teacher__school=user.teacher.school
+        ).first()
+        assert admin_school_teacher_user
+
+        self.assert_get_queryset(
+            action="destroy",
+            values=list(
+                user.auth_factors.all()
+                | admin_school_teacher_user.auth_factors.all()
+            ),
+            request=self.client.request_factory.get(
+                user=admin_school_teacher_user
+            ),
+        )
+
+    def test_get_queryset__destroy__non_admin(self):
+        """Can only destroy your own auth factors if you are not an admin."""
+        user = self.mfa_non_admin_school_teacher_user
+
+        self.assert_get_queryset(
+            action="destroy",
+            values=list(user.auth_factors.all()),
+            request=self.client.request_factory.get(user=user),
+        )
+
+    def test_get_queryset__generate_otp_provisioning_uri(self):
+        """Can only generate an OTP provisioning URI yourself."""
+        user = self.mfa_non_admin_school_teacher_user
+
+        self.assert_get_queryset(
+            action="generate_otp_provisioning_uri",
             values=list(user.auth_factors.all()),
             request=self.client.request_factory.get(user=user),
         )
@@ -85,7 +133,13 @@ class TestAuthFactorViewSet(ModelViewSetTestCase[User, AuthFactor]):
         """Only a teacher-user can disable an auth factor."""
         self.assert_get_permissions([IsTeacher()], action="destroy")
 
-    # test: generic actions
+    def test_get_permissions__generate_otp_provisioning_uri(self):
+        """Only a teacher-user can generate a OTP provisioning URI."""
+        self.assert_get_permissions(
+            [IsTeacher()], action="generate_otp_provisioning_uri"
+        )
+
+    # test: actions
 
     def test_list(self):
         """Can list enabled auth-factors."""
@@ -96,14 +150,23 @@ class TestAuthFactorViewSet(ModelViewSetTestCase[User, AuthFactor]):
 
     def test_create__otp(self):
         """Can enable OTP."""
-        teacher_user = TeacherUser.objects.filter(
-            auth_factors__isnull=True
+        teacher_user = TeacherUser.objects.exclude(
+            auth_factors__type__in=["otp"]
         ).first()
         assert teacher_user
 
+        # TODO: make "otp_secret" non-nullable and delete code block
+        teacher_user.userprofile.otp_secret = pyotp.random_base32()
+        teacher_user.userprofile.save(update_fields=["otp_secret"])
+
         # TODO: set password="password" on all user fixtures
         self.client.login_as(teacher_user, password="abc123")
-        self.client.create({"type": "otp"})
+        self.client.create(
+            {
+                "type": AuthFactor.Type.OTP,
+                "otp": teacher_user.totp.now(),
+            }
+        )
 
     def test_destroy(self):
         """Can disable an auth-factor."""
@@ -113,3 +176,28 @@ class TestAuthFactorViewSet(ModelViewSetTestCase[User, AuthFactor]):
 
         self.client.login_as(user)
         self.client.destroy(auth_factor)
+
+    def test_generate_otp_provisioning_uri(self):
+        """Can successfully generate a OTP provisioning URI."""
+        user = TeacherUser.objects.exclude(
+            auth_factors__type__in=[AuthFactor.Type.OTP]
+        ).first()
+        assert user
+
+        # TODO: normalize password to "password"
+        self.client.login_as(user, password="abc123")
+
+        with patch.object(
+            TOTP, "provisioning_uri", return_value=user.totp_provisioning_uri
+        ) as provisioning_uri:
+            response = self.client.post(
+                self.reverse_action("generate_otp_provisioning_uri")
+            )
+
+            provisioning_uri.assert_called_once_with(
+                name=user.email,
+                issuer_name="Code for Life",
+            )
+
+            assert response.data == provisioning_uri.return_value
+            assert response.content_type == "text/plain"
