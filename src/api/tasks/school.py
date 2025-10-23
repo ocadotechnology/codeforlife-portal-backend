7811cda901bfd6e0049ad681730f0079ea84e13e
SKairinos <skairinos@gmail.com>
2025-10-23 16:50:22 +0000

    teachers_per_school

--- src/api/tasks/school.py
+++ src/api/tasks/school.py
@@ -5,6 +5,7 @@ Created on 23/10/2025 at 17:13:48(+01:00).
 
 from codeforlife.tasks import DataWarehouseTask
 from codeforlife.user.models import School
+from django.db.models import Count
 
 
 @DataWarehouseTask.shared(
@@ -22,3 +23,26 @@ def common_school():
     https://console.cloud.google.com/bigquery?tc=europe:60643198-0000-2efe-8b5f-f403043816d8&project=decent-digit-629&ws=!1m0
     """
     return School.objects.all()
+
+
+@DataWarehouseTask.shared(
+    DataWarehouseTask.Settings(
+        bq_table_write_mode="overwrite",
+        chunk_size=1000,
+        fields=["id", "name", "country", "teacher_count"],
+    )
+)
+def teachers_per_school():
+    """
+    Collects data about the School table, and counting how many Teacher rows are
+    related to the School object. Used to report on metrics like average and max
+    number of teachers per school.
+
+    https://console.cloud.google.com/bigquery?tc=europe:608bfedc-0000-2064-9e7f-94eb2c139c38&project=decent-digit-629&ws=!1m0
+    """
+    return (
+        School.objects.values("id", "name", "country")
+        .annotate(teacher_count=Count("teacher_school"))
+        .filter(teacher_count__gt=0)  # Caveat: Mimics INNER JOIN of SQL query
+        .order_by("-teacher_count")
+    )
--- src/api/tasks/school_test.py
+++ src/api/tasks/school_test.py
@@ -5,7 +5,7 @@ Created on 23/10/2025 at 17:13:51(+01:00).
 
 from codeforlife.tests import CeleryTestCase
 
-from .school import common_school
+from .school import common_school, teachers_per_school
 
 # pylint: disable=missing-class-docstring
 
@@ -16,3 +16,7 @@ class TestSchool(CeleryTestCase):
     def test_common_school(self):
         """Assert the queryset returns the expected fields."""
         self.assert_data_warehouse_task(task=common_school)
+
+    def test_teachers_per_school(self):
+        """Assert the queryset returns the expected fields."""
+        self.assert_data_warehouse_task(task=teachers_per_school)
