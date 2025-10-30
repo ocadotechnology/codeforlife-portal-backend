2eb43bfda24998986818e1248da8eeaf03eb4b88
SKairinos <skairinos@gmail.com>
2025-10-30 17:23:33 +0000

    fix

--- src/rapid_router/models/top_score.py
+++ src/rapid_router/models/top_score.py
@@ -3,6 +3,6 @@
 Created on 30/10/2025 at 17:08:12(+00:00).
 """
 
-# pylint: disable-next=unused-import
 # TODO: rename this model to TopScore
+# pylint: disable-next=unused-import
 from game.models import Attempt  # type: ignore[import-untyped]
